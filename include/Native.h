1ddc40b6ad92f4d301afd89aff4f93eb2461f0af
coremedic <ben@aptlabs.net>
2024-01-29 21:15:01 -0500

    _SYSTEM_PROCESS_INFOMATION struct

--- include/Native.h
+++ include/Native.h
@@ -8,6 +8,25 @@ typedef NTSTATUS(NTAPI* tNtQuerySystemInformation)(
     PVOID SystemInformation,
     ULONG SystemInformationLength,
     PULONG ReturnLength
-);
+    );
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+} UNICODE_STRING;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG NumberOfThreads;
+    ULONG Reserved1[6];
+    LARGE_INTEGER CreateTime;
+    LARGE_INTEGER UserTime;
+    LARGE_INTEGER KernelTime;
+    UNICODE_STRING ImageName;
+    ULONG BasePriority;
+    HANDLE ProcessId;
+    HANDLE InheriredFromProcessId;
+} SYSTEM_PROCESS_INFORMATION, *PSYSTEM_PROCESS_INFORMATION;
 
 #endif //NATIVE_H
